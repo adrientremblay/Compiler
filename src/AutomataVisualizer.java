bea9025b868e64d7621273163a0fe2c56cf92681
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-18 14:26:28 -0500

    Started on actual conversion code

--- src/AutomataVisualizer.java
+++ src/AutomataVisualizer.java
@@ -2,10 +2,12 @@ import guru.nidi.graphviz.attribute.Color;
 import guru.nidi.graphviz.engine.Format;
 import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.model.MutableGraph;
+import guru.nidi.graphviz.model.Node;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Stack;
 
 import static guru.nidi.graphviz.model.Factory.*;
@@ -17,22 +19,51 @@ public class AutomataVisualizer {
     public void visualize(State entry) throws IOException {
         MutableGraph graph = mutGraph("example1").setDirected(true);
 
-        Stack<State> stateStack = new Stack<State>();
-        stateStack.add(entry);
-
         int i = 1;
-        while (!stateStack.empty()) {
-            State state = stateStack.pop();
-            // add this state to the graph
-            graph.add(node(String.valueOf(i++)));
+        Stack<StateNodePair> graphStack = new Stack<StateNodePair>();
+        Node baseNode = node(String.valueOf(i++));
+        graphStack.add(new StateNodePair(entry, baseNode));
+        HashMap<State, Node> seen = new HashMap<State, Node>();
+        seen.put(entry, baseNode);
 
-            // add it's children to the stack
-            HashMap<String, State> stateEdges = state.getEdges();
+        while (!graphStack.empty()) {
+            StateNodePair curPair = graphStack.pop();
+            // add this state to the graph
+            graph.add(curPair.node);
+
+            HashMap<String, State> stateEdges = curPair.state.getEdges();
             for (String label: stateEdges.keySet()) {
-                stateStack.push(stateEdges.get(label));
+                State childState = stateEdges.get("label");
+
+                Node childNode;
+                if (seen.containsKey(curPair.state))
+                    childNode = seen.get(curPair.state);
+                else
+                    childNode = node(String.valueOf(i++));
+
+                StateNodePair childPair = new StateNodePair(childState, childNode);
+
+                // draw edge
+                // todo: use label
+                curPair.node.link(childPair.node);
+
+                // add the child to the stack
+                if (!seen.containsKey(curPair.state))
+                    graphStack.push(childPair);
+
             }
         }
 
         Graphviz.fromGraph(graph).width(500).render(Format.PNG).toFile(new File("example/ex1m.png"));
+    }
+
+    private class StateNodePair {
+        public State state;
+        public Node node;
+
+        public StateNodePair(State state, Node node) {
+            this.state = state;
+            this.node = node;
+        }
     }
 }
