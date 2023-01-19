eef7ac45d48e383382d06f1e21919351fab7b033
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-19 14:41:53 -0500

    Make epsilonClosure more expansive but DFA still not generating correctly

--- src/State.java
+++ src/State.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 // todo: state should really be an intercae cause for DfaState edges should be only other Dfa States
 public class State {
@@ -20,13 +21,23 @@ public class State {
 
     public Set<State> epsilonClosure() {
         Set<State> ret = new HashSet<State>();
-        ret.add(this);
 
-        for (Edge edge : edges) {
-            if (edge.label != "ε")
-                continue;
+        Stack<State> toExplore = new Stack<State>();
+        toExplore.add(this);
 
-            ret.add(edge.destination);
+        while (! toExplore.empty()) {
+            State state = toExplore.pop();
+            ret.add(state);
+
+            for (Edge edge : state.edges) {
+                if (edge.label != "ε")
+                    continue;
+
+                if (toExplore.contains(edge.destination) || ret.contains(edge.destination))
+                    continue;
+
+                toExplore.push(edge.destination);
+            }
         }
 
         return ret;
