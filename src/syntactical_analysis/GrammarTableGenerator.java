32ee4e92568c70e89ef50a923b1ec9ab8ad067ae
Adrien Tremblay <adrien@adrientremblay.com>
2023-02-14 16:35:14 -0500

    Read rules

--- src/syntactical_analysis/GrammarTableGenerator.java
+++ src/syntactical_analysis/GrammarTableGenerator.java
@@ -4,14 +4,17 @@ import lexical_analysis.Token;
 import util.Util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 // todo: right know the grammar table is generated dynamically, implement saving and loading later
 public class GrammarTableGenerator {
     HashMap<String, HashMap<Token, String>> grammarTable;
-    ArrayList<HashMap<String, ArrayList<String>>> rules;
+    HashMap<String, ArrayList<String[]>> rules;
+
+    HashMap<String, ArrayList<String>> firstSets;
+    HashMap<String, ArrayList<String>> followSets;
 
     public HashMap<String, HashMap<Token, String>> generateGrammarTable() {
         HashMap<String, HashMap<Token, String>> grammarTable = new HashMap<String, HashMap<Token, String>>();
@@ -20,9 +23,29 @@ public class GrammarTableGenerator {
         List<String> lines = Util.readFileForLines("grammar/grammar.grm");
 
         // Reading rules
+        rules = new HashMap<String, ArrayList<String[]>>();
+
         for (String line : lines) {
-            System.out.println(line);
+            if (line.length() == 0)
+                continue;
+
+            String[] stringSplit = line.split(" ");
+
+            if (stringSplit.length < 3 || stringSplit[0].length() == 0 || (!stringSplit[1].equals("->"))) {
+                System.err.println("Invalid rule : " + line);
+                continue;
+            }
+
+            String leftHandSide = stringSplit[0];
+            String[] rightHandSide = Arrays.<String>copyOfRange(stringSplit, 2, stringSplit.length);
+
+            if (!rules.containsKey(leftHandSide))
+                rules.put(leftHandSide, new ArrayList<String[]>());
+
+            rules.get(leftHandSide).add(rightHandSide);
         }
+
+        // Generating first sets
 
         // Parse the grammar file
 
@@ -30,7 +53,10 @@ public class GrammarTableGenerator {
         return null;
     };
 
-    private static void handleRule(HashMap<String, HashMap<Token, String>> grammarTable, String rule) {
+    private static boolean isTerminal(String s) {
+        if (s.length() <= 2)
+            return false;
 
+        return (s.charAt(0) == '\'' && s.charAt(s.length() - 1) == '\'');
     }
 }
