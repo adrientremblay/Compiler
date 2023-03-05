5cc4dcea4c1e8aa3b8c166adf452a51eb5b8e3d7
Adrien Tremblay <adrien@adrientremblay.com>
2023-03-05 10:04:18 -0500

    Add bubble sort extended

--- src/Driver.java
+++ src/Driver.java
@@ -11,11 +11,11 @@ public class Driver {
 
     private static final String[] SOURCE_FILES = {
             //        "test_source_files/my_test/my_test.src",
-            //          "test_source_files/bubble_sort/bubble_sort.src",
+                      "test_source_files/bubble_sort/bubble_sort.src",
             //        "test_source_files/lex_negative_grading/lex_negative_grading.src",
             //        "test_source_files/lex_positive_grading/lex_positive_grading.src",
             //        "test_source_files/polynomial/polynomial.src"
-            "test_source_files/syntax_errors_galore/syntax_errors_galore.src",
+            //"test_source_files/syntax_errors_galore/syntax_errors_galore.src",
     };
 
     public static void main(String args[]) throws IOException {
