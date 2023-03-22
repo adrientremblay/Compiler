612cc1d7de13bc3f3b03d3a5a863726eb2281bd8
Adrien Tremblay <adrien@adrientremblay.com>
2023-03-22 09:06:52 -0400

    Extract attribs in the ClassDeclaration semantic concept

--- src/syntactical_analysis/ast_generation/tree/classes/ClassDeclaration.java
+++ src/syntactical_analysis/ast_generation/tree/classes/ClassDeclaration.java
@@ -1,13 +1,24 @@
 package syntactical_analysis.ast_generation.tree.classes;
 
+import syntactical_analysis.ast_generation.tree.Identifier;
 import syntactical_analysis.ast_generation.tree.SemanticConcept;
 import semantic_analysis.SymbolTableVisitor;
 
+import java.util.IdentityHashMap;
+
 public class ClassDeclaration extends SemanticConcept {
-    public ClassDeclaration(SemanticConcept id, SemanticConcept inheritanceList, SemanticConcept memberList) {
+    private MemberList memberList;
+    private InheritanceList inheritanceList;
+    private Identifier identifier;
+
+    public ClassDeclaration(SemanticConcept memberList, SemanticConcept inheritanceList, SemanticConcept id) {
         addChild(id);
         addChild(inheritanceList);
         addChild(memberList);
+
+        this.memberList = (MemberList) memberList;
+        this.inheritanceList = (InheritanceList) inheritanceList;
+        this.identifier = (Identifier) id;
     }
 
     @Override
